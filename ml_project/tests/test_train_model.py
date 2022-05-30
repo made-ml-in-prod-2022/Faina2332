1c92eb65ff7942a026cfc342ae6884f3a7d99a09
Alimbaeva Faina <66172176+faina2332@users.noreply.github.com>
2022-05-30 15:39:16 +0600

    Fixed tests,.gitignore

--- ml_project/tests/test_train_model.py
+++ ml_project/tests/test_train_model.py
@@ -3,8 +3,8 @@ from typing import NoReturn, Tuple
 
 from sklearn.ensemble import RandomForestClassifier
 
-from src.models import train
-from src.entities import TrainParams
+from ml_project.src.models import train
+from ml_project.src.entities import TrainParams
 
 
 def test_train_model(
@@ -13,6 +13,11 @@ def test_train_model(
 ) -> NoReturn:
     target, transformed_dataset = transformed_dataframe
     model = train(transformed_dataset, target, training_params)
+    params = model.get_params()
+
     assert isinstance(model, RandomForestClassifier)
+    assert params['max_depth'] == 10
+    assert params['n_estimators'] == 50
+    assert params['criterion'] == 'gini'
 
 
